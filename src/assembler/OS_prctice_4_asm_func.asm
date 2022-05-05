918607e547407a18da31279e8683f3a29d5353d0
Kostya Bespalov <96658739+PredsedateI@users.noreply.github.com>
2022-05-05 14:14:21 +0300

    Add files via upload

--- src/assembler/OS_prctice_4_asm_func.asm
+++ src/assembler/OS_prctice_4_asm_func.asm
@@ -1,13 +1,34 @@
 .DATA
-    temp dq ?
+    constbasic dq ?
+    constb dq ?
+    constc dq ?
+    i dq ?
+    n dq 1
+    iter dq 100000000
     
 .CODE
 f PROC C
+
+    mov constbasic, rcx
+    mov constb, rdx
+    mov constc, r8
+    mov i, r9
+
     mov rax, 0
-    add rax, rcx
-    add rax, rdx
-    add rax, r8
-    add rax, r9
+    mov     rbx, [constb]
+    add     rbx, rbx
+    add     rbx, [constc]
+loopf:
+    mov     rcx, [iter]
+loopfor:
+    add     rax, rbx
+    sub     rax, [i]
+    loop    loopfor
+
+    dec [i]
+    mov rcx, [i]
+    loop loopf
+
     ret
 f ENDP
 END
